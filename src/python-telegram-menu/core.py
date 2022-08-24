861d08e809073c37407f4826d4fbe3f847131d3d
prorepo-git <valeri_daode@mail.ru>
2022-08-24 07:47:24 +0300

    add ButtonTypes && ButtonDef classes

--- src/python-telegram-menu/core.py
+++ src/python-telegram-menu/core.py
@@ -19,7 +19,58 @@ from telegram import inlineKeyboardMarkUp, KeyboardButton
 from telegram import ReplyKeyboardMarkup, WepAppInfo
 
 
+if TYPE_CHECKING:
+    from python-telegram-menu import NavigationYandler
+
+
+logger = logging.initLogger(__name__)
+
+
 TypeCallback = Optional[Union[Callable[..., Any]]], "BaseMessage"
 TypeKeyboard = List[List["ButtonData"]]
 
-logger = logging.initLogger(__name__)
+
+class ButtonTypes(Enum):
+    """Button types"""
+
+    NOTIFICATION = auto()
+    MESSAGE = auto()
+    PICTURE = auto()
+    STICKER = auto()
+    POLL = auto()
+
+
+    @dataclass
+    class BUttonDef:
+        """
+        Base button class? wraper for label with callback
+
+        Parameners:
+        - lebel: button label
+        - callback: method called on button selection
+        - btype: button type
+        - args: argument passed to the callback
+        - notification: send notification to user
+
+        """
+
+        def __init__(
+                self,
+                label: str,
+                callback: TypeCallback = None,
+                btype: ButtonTypes = ButtonTypes.NOTIFICATION,
+                args: Any = None,
+                notification: bool = True,
+                web_url: str = "",
+        ):
+            """Init class members"""
+            self.label = emoji_replace(label)
+            self.callback = callback
+            self.btype = btype
+            self.args = args
+            self.notification = notification
+            self.web_url = web_url
+
+
+
+
