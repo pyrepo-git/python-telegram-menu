54da06566b67907bf76921717df0937b82e09487
pyrepo-git <49498725+pyrepo-git@users.noreply.github.com>
2022-09-01 15:01:29 +0300

    Update session.py

--- src/python_telegram_menu/session.py
+++ src/python_telegram_menu/session.py
@@ -35,6 +35,7 @@ class Session:
         - message_args: message class args
         - handler: handler class
     """
+    
     TIMEOUT_READ = 5
     TIMEOUT_CONNECT = TIMEOUT_READ
     INIT_STRING = "start"
