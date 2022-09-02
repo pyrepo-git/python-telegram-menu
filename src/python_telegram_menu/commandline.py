64b9a6b5c47f154d51dbbb0c5a44721e19f30fb1
pyrepo-git <49498725+pyrepo-git@users.noreply.github.com>
2022-09-02 11:29:47 +0300

    Update commandline.py

--- src/python_telegram_menu/commandline.py
+++ src/python_telegram_menu/commandline.py
@@ -6,6 +6,7 @@ from python_telegram_menu import __version__
 logging.basicConfig()
 log = logging.getLogger(__name__)
 
+
 @click.command()
 @click.group(context_settings=dict(help_option_names=["-h", "--help"]))
 @click.version_option(version=__version__)
