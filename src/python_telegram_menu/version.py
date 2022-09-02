6b7a20d25ef906acc17081177716ded367c80340
pyrepo-git <49498725+pyrepo-git@users.noreply.github.com>
2022-09-02 11:38:06 +0300

    Create version.py

---
+++ src/python_telegram_menu/version.py
@@ -0,0 +1,18 @@
+"""
+python_telegram_menu package information.
+"""
+
+# Use semantic versioning (https://semver.org/)
+# The version number is controlled through bumpversion.cfg
+# version as tuple for simple comparisons
+VERSION = (0, 0, 1)
+# string created from tuple to avoid inconsistency
+__version__ = ".".join([str(x) for x in VERSION])
+__title__ = "python_telegram_menu"
+__description__ = "A python library to generate menus using Telegram Bot API."
+__url__ = "https://github.com/pyrepo-git/python_telegram_menu"
+__raw_url__ = "https://github.com/pyrepo-git/python_telegram_menu"
+__author__ = "Valeri Zimin"
+__author_email__ = "valerizimin@gmail.com"
+__license__ = "GNU GPLv3"
+__copyright__ = "Copyright 2022-2022 " + __author__
