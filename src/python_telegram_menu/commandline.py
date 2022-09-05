6e6baa1753d2df5bdf7b9982977594055e2662a9
pyrepo-git <49498725+pyrepo-git@users.noreply.github.com>
2022-09-05 12:53:41 +0300

    Update commandline.py

--- src/python_telegram_menu/commandline.py
+++ src/python_telegram_menu/commandline.py
@@ -1,12 +1,12 @@
 import logging
 import click
-import .vertion
+import vertion
 
 
 from importlib import reload
-reload(.vertion)
+reload(vertion)
 
-#from .version import __version__
+#from version import __version__
 
 
 logging.basicConfig()
@@ -14,7 +14,7 @@ log = logging.getLogger(__name__)
 
 
 @click.group(context_settings=dict(help_option_names=["-h", "--help"]))
-@click.version_option(version=.version.__version__)
+@click.version_option(version=version.__version__)
 def main():
     print("Hellow from my menu package!")
 
