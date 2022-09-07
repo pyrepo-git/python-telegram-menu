49b08cab961e0e3085a1d3a0b357cbbffb95d4d2
pyrepo-git <49498725+pyrepo-git@users.noreply.github.com>
2022-09-07 08:00:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
+import codecs
+import os.path
+
 from setuptools import setup
 
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
 
-pkg_version = "0.0.1"
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 extras_require = {
     "develop": [
@@ -24,7 +37,7 @@ extras_require = {
 extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
 
 setup(
-    version=pkg_version,
+    version=get_version("python_telegram_menu/__init__.py"),
     extras_require=extras_require,
     entry_points={
         "console_scripts": [
