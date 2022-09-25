30ab6543f2c6b4daa1484a700f0c1793431015c5
prorepo-git <valeri_daode@mail.ru>
2022-09-25 10:16:27 +0300

    add handler.py

---
+++ tests/hahdler.py
@@ -0,0 +1,13 @@
+from python_telegram_menu import Handler
+
+
+class TestHandler(Handler):
+    """
+    Extended Test Handler with 'Back' command
+    """
+
+    def back(self) -> int:
+        """
+        Return to previous menu keyboard
+        """
+        return self.select_menu_button("Back")
